20af79270d77f691c0154130f90b569e401a260b
Teresa Mereu <teresa@macbookair.home>
2026-04-18 18:09:04 +0200

    ciao ciao ciaaao

---
+++ R/grappa_sborra2.R
@@ -0,0 +1,17 @@
+#' Print an alternative motivational gRappa message
+#'
+#' This function prints a different but equally strong message in the console.
+#'
+#' @return No return value, prints a message
+#' @export
+
+grappa_sborra2 <- function() {
+
+  cat("\n")
+  cat("=========================================\n")
+  cat("GRAPPA NEL CUORE, FUOCO NELLE VENE\n")
+  cat("SPINGI FORTE, NON MOLLARE MAI\n")
+  cat("=========================================\n")
+  cat("\n")
+
+}
